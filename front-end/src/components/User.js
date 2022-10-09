76bddcc00009d4b312f362d8f64db4b8312a6a0b
samiullah997 <samiullahk997@gmail.com>
2022-10-10 00:44:27 +0500

    update User file

--- front-end/src/components/User.js
+++ front-end/src/components/User.js
@@ -10,7 +10,6 @@ const User = () => {
                 <li>User 1</li>
                 <li>User 1</li>
                 <li>User 1</li>
-                <li>User 1</li>
             </ul>
         </div>
     </div>
