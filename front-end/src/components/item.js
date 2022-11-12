0a70afe08e7f085c9643a61e6b2171475165d409
samiullah997 <samiullahk997@gmail.com>
2022-11-13 00:39:18 +0500

    create item file

---
+++ front-end/src/components/item.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const item = () => {
+  return (
+    <div className='container'>
+        <div className='row'>
+            <ul></ul>
+        </div>
+    </div>
+  )
+}
+
+export default item
